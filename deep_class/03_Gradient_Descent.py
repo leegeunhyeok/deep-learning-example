e0aed832728f707d3412fa1cf1119d58107de710
Leegeunhyeok <lghlove0509@naver.com>
2018-09-16 22:39:00 +0900

    Add comment

--- deep_class/03_Gradient_Descent.py
+++ deep_class/03_Gradient_Descent.py
@@ -5,6 +5,8 @@ data = [[2, 81], [4, 93], [6, 91], [8, 97]]
 x_data = [x_row[0] for x_row in data]
 y_data = [y_row[1] for y_row in data]
 
+print(x_data, y_data)
+
 # 기울기 a와 y 절편 b의 값을 임의로 정한다.
 # 단, 기울기의 범위는 0 ~ 10 사이이며 y 절편은 0 ~ 100 사이에서 변하게 한다.
 a = tf.Variable(tf.random_uniform([1], 0, 10, dtype = tf.float64, seed = 0))
@@ -14,7 +16,8 @@ b = tf.Variable(tf.random_uniform([1], 0, 100, dtype = tf.float64, seed = 0))
 y = a * x_data + b
 
 # 텐서플로 RMSE 함수
-rmse = tf.sqrt(tf.reduce_mean(tf.square( y - y_data )))
+# n은 데이터 수, root(sum((yn - y`n)^2) / n)
+rmse = tf.sqrt(tf.reduce_mean(tf.square( y_data - y )))
 
 # 학습률 값
 learning_rate = 0.1
